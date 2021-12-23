7e02c98bc6881fa12c5ab8a670b41872b23f5392
arbazk-09 <85067731+arbazk-09@users.noreply.github.com>
2021-12-23 11:01:06 +0530

    Update MoveNegPos.java

--- Arrays/MoveNegPos.java
+++ Arrays/MoveNegPos.java
@@ -1,33 +1,27 @@
 import java.util.*;
 
 public class MoveNegPos{
+       //using two pointer technique time complexity O(n) and space is O(1)
 	public static void moveNegPos(int[] arr,int n){
 		int l=0,r=n-1;
+		//if left element is negative than increment in left pointer
+		// if right element is positive than decrement in right pointer
+		//if left element is positive and right element is negative than swap it and increment in left pointer and decrement in right pointer
+		//repeat this step until left pointer is less than right pointer
 		while(l<r){
-			if(arr[l]>-1 && arr[r]>-1){
-               r--;
-               continue;
+			if(arr[l]<0){
+               			l++;
 			}
-			if(arr[r]<0 && arr[l]<0){
-				l++;
-				continue;
+			if(arr[r]>=0){
+				r--;
 			}
-			if(arr[l]<0 && arr[r]>=0){
-				l++;r--;
-				continue;
-			}
-            if(arr[l]>-1){
-            	int temp=arr[r];
-            	arr[r]=arr[l];
-            	arr[l]=temp;
-            	l++;
-            }
-            else{
-            	int temp=arr[l];
-            	arr[l]=arr[r];
-            	arr[r]=temp;
-            	r--;
-            }
+          	 	if(arr[l]>-1 && arr[r]<0){
+            			int temp=arr[r];
+            			arr[r]=arr[l];
+            			arr[l]=temp;
+            			l++;
+            			r--;
+            		}           
 		}
 	}
 	public static void main(String[] args) {
